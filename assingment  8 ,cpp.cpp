6bd2fdb605ea1b3baf441c1ce97aa03080813ffb
sreeparna16 <sreeparnasarkar2005@gmail.com>
2025-02-23 12:22:05 +0530

    Add files via upload

---
+++ assingment  8 ,cpp.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+float fahrenheit_to_celsius(float.fahrentheit)
+{
+	return ((fahrenheit-32.0)*5.0/9.0);
+	int main()
+	float f=22;
+	printf("temp to celcius:%0.2f",faherenheit_to_celsius(f));
+	return 0;
+}
