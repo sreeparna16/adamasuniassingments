1ac30a6ef1eb19a628277dacbfddc0098ec71972
sreeparna16 <sreeparnasarkar2005@gmail.com>
2025-03-02 21:51:21 +0530

    Add files via upload

--- assingment33.cpp
+++ assingment33.cpp
@@ -1,34 +1,27 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-	int num, original_num, remainder, result = 0, n = 0;
+#include <stdio.h>
+
+int main() {
+    int num, reversed_num = 0, remainder, original_num;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
     original_num = num;
 
-
-    while (original_num != 0) {
-        original_num /= 10;
-        n++;
+    
+    while (num != 0) {
+        remainder = num % 10;
+        reversed_num = reversed_num * 10 + remainder;
+        num /= 10;
     }
 
-    original_num = num;
-
-    while (original_num != 0) 
-	{
-        remainder = original_num % 10;
-        result += (int)pow(remainder, n);
-        original_num /= 10;
-    }
-
-    if (result == num)
-	 {
-        printf("%d is an Armstrong number.\n", num);
+    
+    if (original_num == reversed_num) {
+        printf("%d is a palindrome number.\n", original_num);
     } else {
-        printf("%d is not an Armstrong number.\n", num);
+        printf("%d is not a palindrome number.\n", original_num);
+    }
+
+    return 0;
 }
-	return 0;
-}
+
