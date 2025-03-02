e7db6264bd36abd423c38fbc8c31c78514004f85
sreeparna16 <sreeparnasarkar2005@gmail.com>
2025-03-02 21:40:06 +0530

    Add files via upload

---
+++ assingment31.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+        sum += remainder;
+        num /= 10;
+    }
+
+    printf("The sum of the digits is: %d\n", sum);
+
+    return 0;
+}
+
+
