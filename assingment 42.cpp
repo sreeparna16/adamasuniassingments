ff1b4af7361b64b6858e6ab2a6a2ccae3adb803e
sreeparna16 <sreeparnasarkar2005@gmail.com>
2025-03-19 10:58:28 +0530

    Add files via upload

---
+++ assingment 42.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int gcd(int a,int b)
+{
+	if(b==0){
+		return a;
+	}else{
+		return gcd(b,a%b);
+	}
+}
+int main()
+{
+	int a,b;
+	printf("enter the numbers:");
+	scanf("%d %d",&a,&b);
+	printf("GCD of %d and %d: %d\n",a,b,gcd(a,b));
+	return 0;
+}
